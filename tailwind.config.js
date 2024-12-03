40caeabe3e83ab725c406bc52d16b5f6602efce7
joohirao <joohirao0413@gmail.com>
2024-12-03 23:07:37 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.ejs"],
+
+  //tailwind files are in views folder and of the format of ejs
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
